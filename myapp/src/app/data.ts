8e5458b906892588bbd8dc2953654ed9e4233112
Thanh Thinh <thanhthinhcntt@gmail.com>
2018-01-01 13:52:48 +0700

    add hero-details

---
+++ myapp/src/app/data.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Tín' },
+  { id: 3, name: 'Tài' },
+  { id: 13, name: 'Thành' },
+  { id: 14, name: 'Tú' },
+  { id: 15, name: 'Khánh' },
+  { id: 16, name: 'Linh' },
+  { id: 17, name: 'Công' },
+  { id: 18, name: 'IT' },
+  { id: 19, name: 'Lam' },
+  { id: 20, name: 'Tam' }
+];
